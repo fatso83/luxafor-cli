3437a762f84fc88d4d447bd4dcc40318579d1321
Carl-Erik Kopseng <carlerik@gmail.com>
2018-03-15 10:27:59 +0100

    Add option of listing colors

--- index.js
+++ index.js
@@ -3,38 +3,51 @@
 "use strict";
 
 const Luxafor = require("luxafor")();
+const program = require("commander");
+const chalk = require("chalk");
+const fs = require("fs");
+const packageJson = JSON.parse(
+  fs.readFileSync(__dirname + "/package.json").toString()
+);
 const _ = require("lodash");
-
+const coloredColor = c => (c in chalk ? chalk[c](c) : c);
 const args = process.argv.slice(2);
 
-switch (args[0]) {
-  case "-c":
-    doCommand(...args.slice(1));
-    break;
-  case "-h":
-  default:
-    printUsage();
+program
+  .version(packageJson.version)
+  .option("-c, --color <color|r,g,b|off>", "Example: `-c red`, `-c 255,0,255`")
+  .option("-l, --list", "Lists available colors")
+  .on("--help", () => {
+    console.log("luxafor-cli -c red ");
+  })
+  .parse(process.argv);
+
+if (program.args.length) {
+  outputError(
+    `You supplied arguments that were not prefixed by an option flag: "${
+      program.args
+    }". Aborting.`
+  );
+  process.exit(1);
 }
 
-function printUsage() {
-  console.log("-c <command> <command arg>   command");
-  console.log("-h                           Show help");
-  console.log(`\nCommands and arguments:
-    off             Turn off Luxafor
-    color <color>   Set color
-    `);
-  console.log(`\nColors: ${_.keys(Luxafor.colors).join(",")}`);
+if (program.list) {
+  console.log(
+    `Available colors: ${_.keys(Luxafor.colors)
+      .map(coloredColor)
+      .join(", ")}`
+  );
+  process.exit(0);
 }
 
-function doCommand(command, ...args) {
+if (program.color) {
+  setColor(program.color);
+}
+
+function setColor(color, ...args) {
   Luxafor.init(function() {
-    if (command === "color" || command === "off") {
-      const color = command === "off" ? "off" : args[0];
-      Luxafor.setLuxaforColor(Luxafor.colors[color], function() {
-        console.log("done");
-      });
-    } else {
-      console.log("No matching command");
-    }
+    Luxafor.setLuxaforColor(Luxafor.colors[color], function() {
+      console.log(`Set color to ${coloredColor(color)}`);
+    });
   });
 }
